b4d4ce36f6dc865e80db488e32cc11ebccf34c40
Marcel <marcel.lin@student-cs.fr>
2025-05-10 12:04:45 +0200

    chore: re-add tailwind.config.ts as a normal file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,33 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0982d8489b9773811fc13d9b2ae6f65a637ee565568cb435515dd843b79d66ca
-size 817
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB', // Primary Blue
+        lightBg: '#F9FAFB', // Very Light Background
+      },
+      boxShadow: {
+        soft: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+      },
+      borderRadius: {
+        DEFAULT: '0.25rem', // Straight corners, but default can be slightly rounded
+        none: '0',
+        sm: '0.125rem',
+        md: '0.375rem',
+        lg: '0.5rem',
+        xl: '0.75rem',
+        '2xl': '1rem',
+        '3xl': '1.5rem',
+        full: '9999px',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config 
