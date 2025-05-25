1a11f34e16775ec7c1e20d0849bc1a24e3bbaa25
Marcel <marcel.lin@student-cs.fr>
2025-05-25 09:48:37 +0200

    Remove unused remote image patterns from next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'resources.ediblearrangements.com', port: '', pathname: '/**' },
-      { protocol: 'https', hostname: 'rescloud.ediblearrangements.com',    port: '', pathname: '/**' },
       { protocol: 'https', hostname: 'images.unsplash.com',                port: '', pathname: '/**' },
       { protocol: 'https', hostname: 'yrasqdvnkyxnhjxftjak.supabase.co', port: '', pathname: '/storage/v1/object/public/automationdfy-assets/**' },
     ],
